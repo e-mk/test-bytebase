77d317078c20afa0b371046dd29fc4f4ce685f2b
Ernest Mkrtchyan <mkrtchyan.ernest@gmail.com>
2025-02-12 13:51:45 +0400

    Update 1739353499_genesis.sql

--- bytebase/1739353499_genesis.sql
+++ bytebase/1739353499_genesis.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS users3 (
     id UUID DEFAULT generateUUIDv4() PRIMARY KEY,
     name String,
-    email String,
     created_at DateTime DEFAULT now()
 ) ENGINE = MergeTree
 ORDER BY id;
